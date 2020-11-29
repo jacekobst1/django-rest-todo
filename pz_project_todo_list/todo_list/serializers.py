510bc500ced53dce13ad6bff98ec4dd9a54bcfd3
Jacek Obst <j.obst@teb-akademia.pl>
2020-11-29 08:09:21 +0100

    Change SnippetSerializer to TodoListSerializer

--- pz_project_todo_list/todo_list/serializers.py
+++ pz_project_todo_list/todo_list/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from pz_project_todo_list.todo_list.models import TodoList
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class TodoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoList
         fields = ['id', 'title', 'created_at', 'updated_at']
--- pz_project_todo_list/todo_list/views.py
+++ pz_project_todo_list/todo_list/views.py
@@ -1,13 +1,13 @@
 from pz_project_todo_list.todo_list.models import TodoList
-from pz_project_todo_list.todo_list.serializers import SnippetSerializer
+from pz_project_todo_list.todo_list.serializers import TodoListSerializer
 from rest_framework import generics
 
 
 class TodoListList(generics.ListCreateAPIView):
     queryset = TodoList.objects.all()
-    serializer_class = SnippetSerializer
+    serializer_class = TodoListSerializer
 
 
 class TodoListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = TodoList.objects.all()
-    serializer_class = SnippetSerializer
+    serializer_class = TodoListSerializer
