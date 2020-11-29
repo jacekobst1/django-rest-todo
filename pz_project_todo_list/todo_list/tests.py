ef805e260a6f4f77ae0a68b1f730f95e70431cf4
Jacek Obst <j.obst@teb-akademia.pl>
2020-11-29 22:09:43 +0100

    Add 2 tests for list endpoints

--- pz_project_todo_list/todo_list/tests.py
+++ pz_project_todo_list/todo_list/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from pz_project_todo_list.todo_list.models import TodoList
 
-# Create your tests here.
+
+class TodoListsTest(APITestCase):
+    def test_create_list(self):
+        title = 'Test list create'
+
+        response = self.client.post(reverse('todo-lists-list'),
+                                    {'title': title},
+                                    format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TodoList.objects.count(), 1)
+        self.assertEqual(TodoList.objects.get().title, title)
+
+    def test_update_list(self):
+        todo_list = TodoList()
+        todo_list.title = 'Test list update'
+        todo_list.save()
+
+        new_title = 'THIS IS NEW TITLE'
+
+        response = self.client.put(reverse('todo-lists-detail', args=[todo_list.id]),
+                                   {'title': new_title},
+                                   format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(TodoList.objects.count(), 1)
+        self.assertEqual(TodoList.objects.get().title, new_title)
--- pz_project_todo_list/todo_list/urls.py
+++ pz_project_todo_list/todo_list/urls.py
@@ -8,10 +8,10 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('docs/', include_docs_urls(title='Todo list')),
-    path('todo-lists/', views.TodoListList.as_view()),
-    path('todo-lists/<int:pk>/', views.TodoListDetail.as_view()),
-    path('todo-items/', views.TodoItemList.as_view()),
-    path('todo-items/<int:pk>/', views.TodoItemDetail.as_view()),
+    path('todo-lists/', views.TodoListList.as_view(), name='todo-lists-list'),
+    path('todo-lists/<int:pk>/', views.TodoListDetail.as_view(), name='todo-lists-detail'),
+    path('todo-items/', views.TodoItemList.as_view(), name='todo-items-list'),
+    path('todo-items/<int:pk>/', views.TodoItemDetail.as_view(), name='todo-items-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
